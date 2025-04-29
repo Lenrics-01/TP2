650d9613364aca9e85206522fcac1402a0acc743
Christian Eric <e2479537@cmaisonneuve.qc.ca>
2025-04-28 23:34:36 -0400

    creation du fichier save_numpy.py

--- TP2/scripts/save_numpy.py
+++ TP2/scripts/save_numpy.py
@@ -2,5 +2,5 @@ import csv
 import numpy as np # type: ignore
 with open("data/fibonacci.csv", "w", encoding="utf-8") as f:
     tab=[1, 2, 3, 5, 8] 
-    np.savetxt(tab)
+    np.savetxt("tes.out", tab, delimiter=",")
     f.writerow(tab)
